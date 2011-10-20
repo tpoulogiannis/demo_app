f6a2305428d9c33087c1a9031463199d56852d80
poulidis <poulidis@pornflake.(none)>
2011-10-20 22:57:01 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8233350244807615d08b61aaa6dd203646f47e3efe6803e39f8801f610d90723b23ad4337d333b7fa31d1522de477cd8542bd2386b13da40b316717caaf51c91'
